2afec9e4032833b6945ab02d1de44eb5afd1b60d
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-05-17 18:45:29 +0300

    get sorgusu ile api dataları çekildi

--- api1/src/App.js
+++ api1/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import SearchHeader from "./SearchHeader";
+import searchImages from "./api";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState([])
+
+  const handleSubmit = async (term) => {
+    debugger;
+    console.log(term);
+    const result = await searchImages(term);
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchHeader search={handleSubmit} />
     </div>
   );
 }
