5f4216403844eab7ecf90d60195c3af950048d68
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-05-19 15:11:43 +0300

    apiden çekilen imagelar ekrana bastırıldı ve proje tamamlandı

--- api1/src/App.js
+++ api1/src/App.js
@@ -3,20 +3,19 @@ import "./App.css";
 import SearchHeader from "./SearchHeader";
 import searchImages from "./api";
 import { useState } from "react";
+import ImageList from "./components/ImageList";
 
 function App() {
-  const [image, setImage] = useState([])
-
+  const [images, setImages] = useState([])
   const handleSubmit = async (term) => {
-    debugger;
-    console.log(term);
     const result = await searchImages(term);
-    
+    setImages(result);
   };
 
   return (
     <div className="App">
       <SearchHeader search={handleSubmit} />
+      <ImageList imagesPlaceholder={images}/>
     </div>
   );
 }
