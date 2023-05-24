52816ddf2f0d00ff01bf7433b38133b2060bbd15
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-05-24 15:50:54 +0300

    deleteButton aktive edildi

--- task/jiraApplication/src/components/TaskShow.js
+++ task/jiraApplication/src/components/TaskShow.js
@@ -1,5 +1,9 @@
-function TaskShow({ task }) {
-  console.log(task);
+function TaskShow({ task, onDelete }) {
+  
+  const handleClickDelete = ()=>   {
+    onDelete(task.id);
+  }
+
   return (
     <div className="task-show">
       <h4 className="taskList-title" >Görev</h4>
@@ -7,7 +11,7 @@ function TaskShow({ task }) {
       <h4 className="taskList-title"> Yapılacak </h4>
       <p className="taskList-todo"> {task.taskDesc} </p>
       <div className="button-bar" >
-        <button className="button-del" >Sil</button>
+        <button className="button-del" onClick={handleClickDelete}  >Sil</button>
         <button className="button-upd">Güncelle</button>
       </div>
     </div>
